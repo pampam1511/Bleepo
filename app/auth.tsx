aba09ef2cc26c1fefc7f838231c4f112604c2897
pampam1511 <pamela26emma@icloud.com>
2026-01-28 13:57:14 +0000

    im done with the page sign in and sign up page now is to make it pretty

--- app/auth.tsx
+++ app/auth.tsx
@@ -2,13 +2,19 @@ import { KeyboardAvoidingView, Platform, View, } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 
 export default function AuthScreen() {
-    //const[isSignUp, setIsSignUp] = useState();
-    return <KeyboardAvoidingView 
-    behavior={Platform.OS === "ios" ? "padding" : "height"} 
-    >
-        <View>
-            <Text> Create account </Text>
+    const[isSignUp, setIsSignUp] = useState<boolean>(false);
 
+    const handleSwitchMode = () => {
+        setIsSignUp((prev) => !prev);
+
+    };
+
+
+
+    return (
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} >
+         <View>
+            <Text> {isSignUp ? "Create account" : "Welcome Back" }</Text>
             <TextInput 
             label="Email" 
             autoCapitalize="none" 
@@ -16,20 +22,23 @@ export default function AuthScreen() {
             placeholder="example@gmail.com" 
             mode="outlined"
             />
-
-<TextInput 
+            
+            <TextInput 
             label="Password" 
             autoCapitalize="none" 
             keyboardType="email-address" 
             mode="outlined"
             />
+            
+            <Button mode="contained">{isSignUp ? "Sign up" : "Sign in" }</Button>
+            
+            <Button mode="text" onPress={handleSwitchMode}>{isSignUp ? "Have an account? Sign in" : "Dont have an account? Sign up"}</Button>
+            
+            </View>
+            
+            </KeyboardAvoidingView>
 
-            <Button mode="contained">Sign up</Button>
-
-            <Button mode="text">Have an account? Sign in</Button>
-
-        </View>
-
-    </KeyboardAvoidingView>
+    );
+   
 
 }
