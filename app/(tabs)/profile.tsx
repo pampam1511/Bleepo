a30e74721da8ba085a7087722bc880e5dc31f196
pampam1511 <pamela26emma@icloud.com>
2026-01-30 16:43:05 +0000

    massive update just finished the auth it just need tweaking with the UI

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,13 @@
+import { useAuth } from "@/lib/auth-context";
 import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function profileScreen() {
+    const {signOut} = useAuth()
     return (<View>
         
         <Text> hiii this is the profile page</Text>
+        <Button mode="text" onPress={signOut} icon={"logout"}>Sign out</Button>
     </View>
     );
 
--- lib/auth-context.tsx
+++ lib/auth-context.tsx
@@ -1,16 +1,38 @@
-import { createContext } from "react";
-import { Models, ID } from "react-native-appwrite"
+import { createContext, useContext, useEffect, useState } from "react";
+import { ID, Models } from "react-native-appwrite";
 import { account } from "./appwrite";
 
 type AuthContextType ={
-//    user: Models.User<Models.Preferences> | null;
+    user: Models.User<Models.Preferences> | null;
+    isLoadingUser: boolean;
     signUp: (email: string, password: string) => Promise<string | null>;
     signIn: (email: string, password: string) => Promise<string | null>;
+    signOut: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export function AuthProvider({children}:{ children: React.ReactNode}){
+    const [user, setUser] = useState<Models.User<Models.Preferences> | null>(null);
+    //const [loading, setLoading] = useState(true);
+
+    const [isLoadingUser, setIsLoadingUser] = useState<boolean>(true);
+    useEffect(() => {
+        getUser();
+
+    }, [])
+
+    const getUser =async () => {
+        try{
+            const session = await account.get();
+            setUser(session)
+        } catch(error){
+            setUser(null)
+        } finally{
+            setIsLoadingUser(false);
+        }
+        
+    }
 
     const signUp = async (email: string, password: string) => {
         try {
@@ -30,6 +52,8 @@ export function AuthProvider({children}:{ children: React.ReactNode}){
     const signIn = async (email: string, password: string) => {
         try {
             await account.createEmailPasswordSession(email,password);
+            const session = await account.get();
+            setUser(session);
             return null
 
         } catch (error) {
@@ -41,19 +65,31 @@ export function AuthProvider({children}:{ children: React.ReactNode}){
         }
     };
 
+    const signOut = async () => {
+        try {
+            await account.deleteSession("current");
+            setUser(null);
+        } catch(error) {
+            console.log(error);
+        }
+        
+    };
+
     return ( 
-        <AuthContext.Provider value={{ signUp, signIn}}> 
-
-
+        <AuthContext.Provider value={{ user,isLoadingUser, signUp, signIn, signOut}}> 
             {children}
-
-
         </AuthContext.Provider>
         );
 
 }
 
 export function useAuth(){
+    const context = useContext(AuthContext)
+    if (context === undefined) {
+        throw new Error(" useAuth must be inside of the AuthProvider");
+        
+    }
+    return context;
 
 
 }
